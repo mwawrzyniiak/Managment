322c1323212871a39c4c58d25a1858d0ea171d8b
zxcv22z <35369071+zxcv22z@users.noreply.github.com>
2019-02-11 11:01:15 +0100

    Switch project to maven project

---
+++ entity/Product.java
@@ -0,0 +1,63 @@
+package entity;
+
+public class Product {
+    private Long id;
+    private String productName;
+    private float price;
+    private float weight;
+    private String color;
+    private Integer productCount;
+
+    public Product(Long id, String productName, float price, float weight, String color, Integer productCount) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.weight = weight;
+        this.color = color;
+        this.productCount = productCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getProductCount() {
+        return productCount;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setProductCount(int productCount) {
+        this.productCount = productCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                ", weight=" + weight +
+                ", color='" + color + '\'' +
+                ", productCount=" + productCount +
+                '}';
+    }
+}
